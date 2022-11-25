1374ea3115d37a65a9af47f2c2daad01880da2a9
Yasar Kakdas <ykakdas@gmail.com>
2022-11-25 16:42:22 -0500

    Reformatted code, updated readme, added sample input file.

--- README.md
+++ README.md
@@ -3,9 +3,10 @@
 This script is to distinguish real entries from fake ones.
 
  A real entry:
-- Starts with a ‘yyyy-mm-dd’ formatted date,
+- Starts with a ‘yyyy-mm-dd’ formatted date, year should only be 19xx or 20xx
 - Continues with a valid user name,
 - Ends with an IPv4 address that does not start with ‘10’.
+- There should be a single whitespace after date and user name
 
 A valid user name meets the following conditions:
 - Starts with ‘_’ (underscore) or ‘.’ (dot) character,
@@ -18,6 +19,12 @@ IPv4 address:
  numbers is separated by the dots into four sections.
 - Each number can be in the range of [0,255].
 
-If the input file’s name is entries.txt, the program should be run as:
+## Running instructions
 
-$ python extract_entries.py entries.txt
+-   File's path should be specified in command line arguments
+
+-   If the input file’s name is `entries.txt` and located at the same directory that script exists, then the program should be run as:
+
+```
+python extract_entries.py entries.txt
+```
--- extract_entries.py
+++ extract_entries.py
@@ -1,29 +1,44 @@
 # 161101014, Yasar Can Kakdas, Bil 334
-#!/usr/bin/env python
-
-import sys
+# !/usr/bin/env python
+import os
 import re
+import sys
 
-entries_file=sys.argv.pop();
-output_file=open(entries_file[:-4]+'_output.txt','w');
+if not len(sys.argv) > 1:
+    print("Could not find a text file that contains entries")
+    exit(0)
 
-pattern='^((19\d\d)|(20\d\d))-(0[1-9]|10|11|12)-(0[1-9]|1\d|2\d|30) [_.]([A-Z]|[a-z]){3}\d{1,}[/]{0,1} ' \
-        '(?!10\.)(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$';
-#((19\d\d)|(20\d\d)) : for 19xx and 20xx
-#(0[1-9]|10|11|12) : for mounth between 01-12
-#(0[1-9]|1\d|2\d|30) : for day between 01-30
-#[_.] : user name starts with _ or .
-#([A-Z]|[a-z]){3} : at least 3 letter
-#\d{1,} : at least one digit
-#[/]{0,1} : optionally
-#(?!10\.) : ensures that ip address is not start with 10
-#(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.) : one length ip address from 0-9 or 2 length ip address between
-#10-99 or 3 length ip address between 100-255
-#{3} : for xxx.xxx.xxx.
-#last one should come without '.'
-for line in open(entries_file) :
-    result=re.findall(pattern,line);
-    if(len(result)!=0) :
-      lastResult=re.sub('(\d{4})-(\d{2})-(\d{2})',r'\3.\2.\1',line);
-      output_file.write(lastResult);
-output_file.close();
+entries_file = sys.argv.pop()
+
+if not os.path.exists(entries_file):
+    print("Could not find the specified file named: " + entries_file)
+    exit(0)
+
+output_file = open(entries_file[:-4] + '_output.txt', 'w')
+
+date_pattern = '^((19\d\d)|(20\d\d))-(0[1-9]|10|11|12)-(0[1-9]|1\d|2\d|30)'
+username_pattern = '[_.]([A-Z]|[a-z]){3}\d{1,}[/]{0,1}'
+ipv4_pattern = '(?!10\.)(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$'
+
+match_pattern = date_pattern + ' ' + username_pattern + ' ' + ipv4_pattern
+
+# ((19\d\d)|(20\d\d)) : for 19xx and 20xx
+# (0[1-9]|10|11|12) : for month between 01-12
+# (0[1-9]|1\d|2\d|30) : for day between 01-30
+# [_.] : user name starts with _ or .
+# ([A-Z]|[a-z]){3} : at least 3 letter
+# \d{1,} : at least one digit
+# [/]{0,1} : optional
+# (?!10\.) : ensures that ip address does not start with 10
+# (([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.) : one length ip address from 0-9 or 2 length ip address between
+# 10-99 or 3 length ip address between 100-255
+# {3} : for xxx.xxx.xxx.
+# last one should come without '.'
+
+for line in open(entries_file):
+    line = line.replace("\n", "")
+    result = re.findall(match_pattern, line)
+    if len(result) != 0:
+        lastResult = re.sub('(\d{4})-(\d{2})-(\d{2})', r'\3.\2.\1', line)
+        output_file.write(lastResult + "\n")
+output_file.close()
